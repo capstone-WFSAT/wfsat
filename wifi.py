f404fe73665d62fd1d438308d9681ba92f1f55b6
myeonugi <gimhyeonu020514@gmail.com>
2026-08-22 19:04:49 +0900

    Create wifi.py

---
+++ wifi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Note: This script runs Wifi from within a cloned git repo.
+# The script `bin/wifi` is designed to be run after installing (from /usr/sbin), not from the cwd.
+
+from wifi import wifi
+wifi.main()
